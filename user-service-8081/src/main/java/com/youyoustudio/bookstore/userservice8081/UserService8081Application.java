a04f1449ea4a1ff0789898dbf4c3745530b4d18b
lbxx <476427808@qq.com>
2020-07-07 22:10:55 +0800

    临时提交

---
+++ user-service-8081/src/main/java/com/youyoustudio/bookstore/userservice8081/UserService8081Application.java
@@ -0,0 +1,13 @@
+package com.youyoustudio.bookstore.userservice8081;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserService8081Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserService8081Application.class, args);
+    }
+
+}
---
+++ user-service-8081/src/test/java/com/youyoustudio/bookstore/userservice8081/UserService8081ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youyoustudio.bookstore.userservice8081;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserService8081ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
